f6e029098f2a2ec889f076747a4bff52a2bbe481
loonghe <loooonghe@gmail.com>
2024-05-01 23:20:43 +0800

    feat: 项目引入bearer token自定义方法认证，支持BloomRPC直接通过metadata传token以及http请求header传token，服务端统一在拦截器中验证token

--- main.go
+++ main.go
@@ -21,7 +21,10 @@ import (
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -56,11 +59,44 @@ func ServerValidationUnaryInterceptor(ctx context.Context, req interface{}, info
 	if r, ok := req.(Validator); ok {
 		if err := r.ValidateAll(); err != nil {
 			zaplog.Sugar.Errorf("req validate error %v", err)
-			return nil, err
+			return nil, status.Error(codes.InvalidArgument, err.Error())
 		}
 	}
-
 	return handler(ctx, req)
+}
+
+// token校验拦截插件, 即自定义校验方法
+func ServerTokenValidationUnaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	zaplog.Sugar.Infof("%+v", req)
+	// 拦截普通方法请求，验证Token
+	if err := Check(ctx); err != nil {
+		return nil, err
+	}
+	// 继续处理请求
+	return handler(ctx, req)
+}
+
+// Check 验证token
+func Check(ctx context.Context) error {
+	// 从上下文中获取元数据
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return status.Errorf(codes.Unauthenticated, "获取Token失败")
+	}
+	var (
+		appID     string
+		appSecret string
+	)
+	if value, ok := md["app-id"]; ok {
+		appID = value[0]
+	}
+	if value, ok := md["app-secret"]; ok {
+		appSecret = value[0]
+	}
+	if appID != "grpc_token" || appSecret != "123456" {
+		return status.Errorf(codes.Unauthenticated, "Token无效: app-id=%s, app-secret=%s", appID, appSecret)
+	}
+	return nil
 }
 
 func main() {
@@ -104,6 +140,7 @@ func main() {
 			grpc_opentracing.UnaryServerInterceptor(
 				grpc_opentracing.WithTracer(opentracing.GlobalTracer()),
 			),
+			ServerTokenValidationUnaryInterceptor,
 			ServerValidationUnaryInterceptor,
 		),
 	)
@@ -111,7 +148,28 @@ func main() {
 	helloworldpb.RegisterGreeterServer(s, &server{})
 
 	// gRPC-Gateway mux
-	gwmux := runtime.NewServeMux()
+	// 初始化上下文
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// 请求时,将http header中某些字段转发到grpc上下文, 例如token验证场景，rpc请求直接将token塞进metadata, 服务端即可从ctx解析，如果是http请求，一般是将token塞进header，此时需要在grpc-gateway将指定header转发到grpc上下文
+	inComingOpt := runtime.WithIncomingHeaderMatcher(func(s string) (string, bool) {
+		switch s {
+		// 只需要对需要转发的header进行设置，转到context的字段命名不变
+		case "App-Id", "App-Secret":
+			return s, true
+		default:
+			return "", false
+			// 使用默认处理会在http原始header加grpcgateway-前缀转发到上下文呢
+			// return runtime.DefaultHeaderMatcher(s)
+		}
+	})
+	// 响应后,grpc上下文转发到http头部
+	outGoingOpt := runtime.WithOutgoingHeaderMatcher(func(s string) (string, bool) {
+		return "", false
+	})
+
+	gwmux := runtime.NewServeMux(inComingOpt, outGoingOpt)
 	dops := []grpc.DialOption{
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithUnaryInterceptor(
@@ -120,7 +178,7 @@ func main() {
 			),
 		),
 	}
-	err = helloworldpb.RegisterGreeterHandlerFromEndpoint(context.Background(), gwmux, "127.0.0.1:8091", dops)
+	err = helloworldpb.RegisterGreeterHandlerFromEndpoint(ctx, gwmux, "127.0.0.1:8091", dops)
 	if err != nil {
 		log.Fatalln("Failed to register gwmux:", err)
 	}
